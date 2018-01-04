0709d8c57a5bf6895dd2441fa6d48a16eda9ea04
ericgrosse <ericgrosse1@gmail.com>
2018-01-04 15:08:22 -0500

    Added hot module reloading

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ import 'bulma/css/bulma.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    const NextApp = require('components/App').default
+    ReactDOM.render(
+    <NextApp />,
+    document.getElementById('root')
+    )
+  })
+}
