62dd33c6bce80983c44b37e80e745024d4ab07e5
ericgrosse <ericgrosse1@gmail.com>
2018-01-04 13:39:41 -0500

    Added components folder, Header component, some webpack config

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from 'components/Header'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <h1>Accountify</h1>
+      </div>
+    )
+  }
+}
+
+export default App
