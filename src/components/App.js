9c94f7ae1ebbb124384261492a10419aae850e45
ericgrosse <ericgrosse1@gmail.com>
2018-01-04 16:34:49 -0500

    Added ExpenseList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
+import ExpenseList from 'components/ExpenseList'
 import './App.scss'
+
+const tempData = [
+  {
+    name: 'Costco',
+    price: 35.55,
+    tag: 'groceries',
+    notes: '',
+  },
+  {
+    name: 'Uber',
+    price: 20.00,
+    tag: 'transportation',
+    notes: '',
+  },
+];
 
 class App extends Component {
   render() {
@@ -15,6 +31,10 @@ class App extends Component {
               <h1 className="title has-text-centered">
                 Monthly Expenses
               </h1>
+
+              <ExpenseList
+                items={tempData}
+              />
             </div>
           </div>
       </section>
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { arrayOf, shape, string, number } from 'prop-types'
+import moment from 'moment'
+import './ExpenseList.scss'
+
+const tempData = [
+  {
+    date: 1515101473992,
+    name: 'Costco',
+    price: 35.55,
+    tag: 'groceries',
+    notes: '',
+  },
+  {
+    date: 1515101473992,
+    name: 'Uber',
+    price: 20.00,
+    tag: 'transportation',
+    notes: '',
+  },
+];
+
+class ExpenseList extends Component {
+  render() {
+    const { props } = this
+
+    return (
+      <div className="ExpenseList">
+        
+        {
+          props.items.map((item, index) => (
+            <div key={index} className="expense-item">
+              <p>{ moment(item.date).format() }</p>
+              <p>{ item.name }</p>
+              <p>{ item.price }</p>
+              <p>{ item.tag }</p>
+              <p>{ item.notes }</p>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+ExpenseList.propTypes = {
+  items: arrayOf(shape({
+    date: string.isRequired,
+    name: string.isRequired,
+    price: number.isRequired,
+    tag: string.isRequired,
+    notes: string.isRequired,
+  })).isRequired
+}
+
+export default ExpenseList
